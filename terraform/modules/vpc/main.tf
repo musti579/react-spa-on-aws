0ac1042c906e58dbe1ea948cc9a4b876749d8c55
Kamal M <kamal@Kamals-MacBook-Air.local>
2026-06-03 14:23:01 +0100

    route assosication resorce block created + output for igw ids + rt ids

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -50,3 +50,13 @@ resource "aws_route_table" "public_rt" {
     Name = "public-route-table"
   }
 }
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
--- terraform/modules/vpc/output.tf
+++ terraform/modules/vpc/output.tf
@@ -10,3 +10,19 @@ output "public_subnet_1_id" {
 output "public_subnet_2_id" {
   value = aws_subnet.public_2.id
 }
+
+output "public_subnet_ids" {
+  value = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ]
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.gw.id
+}
+
+output "public_route_table_id" {
+  value = aws_route_table.public_rt.id
+}
+
