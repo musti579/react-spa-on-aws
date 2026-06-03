41edb08c45697d03996a646bdf0b2918faa7fbcc
Kamal M <kamal@Kamals-MacBook-Air.local>
2026-06-03 16:39:39 +0100

    alb target group 'ecs'

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -39,3 +39,26 @@ resource "aws_lb" "app" {
     Name = "threatmod-alb"
   }
 }
+
+
+resource "aws_lb_target_group" "ecs" {
+  name        = "threatmod-tg"
+  port        = 3000
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "threatmod-tg"
+  }
+}
