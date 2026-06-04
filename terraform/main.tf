f1eb66dc1df59d4b6d53aaadf228fa7e433957ad
Kamal M <kamal@Kamals-MacBook-Air.local>
2026-06-04 19:24:25 +0100

    root main.tf update + debug

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,13 @@ module "vpc" {
   source = "./modules/vpc"
 }
 
+module "ecs" {
+  source = "./modules/ecs"
+}
 module "alb" {
   source = "./modules/alb"
 
   vpc_id            = module.vpc.aws_vpc
   public_subnet_ids = module.vpc.public_subnet_ids
 }
+
