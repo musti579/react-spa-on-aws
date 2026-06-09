5de43905f7ee4691bf13fb82958a66372981f11b
Kamal M <kamal@Kamals-MacBook-Air.local>
2026-06-09 13:37:42 +0100

    debugging s3 state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,8 +14,8 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket       = "mustafaabukar-devops-tfstate"
-    key          = "ecs-project/terraform.tfstate"
+    bucket       = "terraform-state-mustafa"
+    key          = "terraform.tfstate"
     region       = "eu-north-1"
     use_lockfile = true
   }
